0724d2abe1793c9432cfd6e8a1234bde1a64bf5a
virupokar <viral.pokar31@gmail.com>
2020-07-21 14:13:28 +0530

    my First Commit react live website

---
+++ build/precache-manifest.5201d20b43b36003fc7b29990ad7e55b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f0c5300f0926b385c4de6cc84be104e",
+    "url": "/First_React_APP/index.html"
+  },
+  {
+    "revision": "615d486fe79ca508d611",
+    "url": "/First_React_APP/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "7b2fb5c901ace69b42fe",
+    "url": "/First_React_APP/static/css/main.432cc2c4.chunk.css"
+  },
+  {
+    "revision": "615d486fe79ca508d611",
+    "url": "/First_React_APP/static/js/2.12a9706f.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/First_React_APP/static/js/2.12a9706f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b2fb5c901ace69b42fe",
+    "url": "/First_React_APP/static/js/main.43ad66bb.chunk.js"
+  },
+  {
+    "revision": "83db787fe975bf21989d",
+    "url": "/First_React_APP/static/js/runtime-main.78820b86.js"
+  },
+  {
+    "revision": "f54b0a0e26c71d992d16cdfa2bfd90bc",
+    "url": "/First_React_APP/static/media/img1.f54b0a0e.jpg"
+  },
+  {
+    "revision": "906408125885fc67c045b261dc83a980",
+    "url": "/First_React_APP/static/media/img3.90640812.jpg"
+  },
+  {
+    "revision": "89608edf25a7ff5b8eed61f05d13b244",
+    "url": "/First_React_APP/static/media/img4.89608edf.jpeg"
+  },
+  {
+    "revision": "569fa192af2a5c9db041826b47b7a576",
+    "url": "/First_React_APP/static/media/img5.569fa192.jpg"
+  },
+  {
+    "revision": "30577c97c1803258d2a90baf8384c284",
+    "url": "/First_React_APP/static/media/img6.30577c97.jpg"
+  },
+  {
+    "revision": "28854b219c18a931fa7702055e938834",
+    "url": "/First_React_APP/static/media/img7.28854b21.jpg"
+  },
+  {
+    "revision": "e9f531f7fb2eb151cfd44fe8991b8cf8",
+    "url": "/First_React_APP/static/media/img8.e9f531f7.jpg"
+  }
+]);
